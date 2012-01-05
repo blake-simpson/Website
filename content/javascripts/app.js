22c8cb3ffc0afd83d6d352e0a461e16e74add8bb
Blake Simpson <simpson@ubilabs.net>
2012-01-05 21:31:03 +0100

    Center popups over markers

--- content/javascripts/app.js
+++ content/javascripts/app.js
@@ -5,6 +5,7 @@ $(function() {
       center_dundee = [56.462018,-2.970721],
       center_hamburg = [53.5569,9.9946],
       default_zoom = 14,
+      markers = [],
       location,
       bounds,
       profile_photo,
@@ -26,6 +27,7 @@ $(function() {
     
     map = new google.maps.Map($canvas.get(0), mapOpts); 
     bounds = new google.maps.LatLngBounds();
+    google.maps.event.addListener(map, 'bounds_changed', function() { update_popups(); });
     load_tweets();
   };
   
@@ -46,21 +48,57 @@ $(function() {
       var lat = tweet.geo.coordinates[0],
           lng = tweet.geo.coordinates[1],
           point = new google.maps.LatLng(lat,lng);
-      
-      create_popup(tweet);    
+          
       marker = new google.maps.Marker({
         map: map,
         position: point,
         tweet_id: tweet.id
       });
+      markers.push(marker);
+      create_popup(tweet, marker);
       click_listener(marker);
       bounds.extend(point);
     });
   }
   
-  function create_popup(tweet) {
+  function update_popups() {
+    $('.popup').each(function() {
+      var $self = $(this);
+      update_popup_position($self.data('id'));
+    });
+  }
+  
+  function update_popup_position(popup_id) { 
+    var $popup = $('.popup[data-id="'+popup_id+'"]');
+    $.each(markers, function(key, marker) {
+      if(marker.tweet_id == popup_id) {
+        var marker_offset = get_marker_offset(marker),
+            offset = popup_position($popup, marker_offset);
+        $popup.css('left', offset.x+'px');
+        $popup.css('top', offset.y+'px');
+      }
+    });
+  }
+  
+  function popup_position(popup, offset) {
+    var x = offset.x,
+        y = offset.y,
+        width = popup.outerWidth(),
+        height = popup.outerHeight();
+    var new_x = x-(width/2);
+    if(height == 62) {
+      var new_y = y-(height*1.6);
+    } else if(height == 48) {
+      var new_y = y-(height*1.8);
+    } else {
+      var new_y = y-(height*1.5);
+    }
+    return {x: new_x, y: new_y}
+  }
+  
+  function create_popup(tweet, marker) {
     var popup = $("<div></div>").appendTo("body");
-    popup.data('id', tweet.id);
+    popup.attr('data-id', tweet.id);
     popup.addClass('popup');
     popup.text(tweet.text);
   }
@@ -89,6 +127,20 @@ $(function() {
       $("aside.profile img").attr('src', profile_photo).show();
       $("aside.profile .loading").hide();
     }
+  }
+  
+  function get_marker_offset(marker) {
+    var scale = Math.pow(2, map.getZoom());
+    var nw = new google.maps.LatLng(
+        map.getBounds().getNorthEast().lat(),
+        map.getBounds().getSouthWest().lng()
+    );
+    var worldCoordinateNW = map.getProjection().fromLatLngToPoint(nw);
+    var worldCoordinate = map.getProjection().fromLatLngToPoint(marker.getPosition());
+    return new google.maps.Point(
+        Math.floor((worldCoordinate.x - worldCoordinateNW.x) * scale),
+        Math.floor((worldCoordinate.y - worldCoordinateNW.y) * scale)
+    );
   }
   
   function add_events() {
